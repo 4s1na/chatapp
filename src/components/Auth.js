daebc0072af56f2130d21e16182b513637c345f3
ash <ashnarafeekvka@gmail.com>
2025-09-22 18:58:43 +0530

    Update Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,22 +1,27 @@
-import {auth, provider} from '../firebase-config'
-import { signInWithPopup } from 'firebase/auth'
-import Cookies from 'universal-cookie'
+import React from "react";
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import Cookies from "universal-cookie";
+import "../styles/Auth.css";
+
 const cookies = new Cookies();
 
-export const Auth = (props) => {
-    const {setIsAuth} = props;
-
-import React from 'react'
-import '../styles/Auth.css'
-export const Auth = () => {
-    const signInWithGoogle = () => {
-        try {
-       const result = await signInWithPopup(auth, provider)
-       console.log(result);
-       cookies.set("auth-token", result.user.refreshToken);
-         setIsAuth(true);
+export const Auth = ({ setIsAuth }) => {
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+      cookies.set("auth-token", result.user.refreshToken);
+      setIsAuth(true);
     } catch (error) {
-    return <div className="auth">
-        <p>Sign in with Google to continue</p>
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      console.error("Google sign-in error:", error);
+    }
+  };
+
+  return (
+    <div className="auth">
+      <p>Sign in with Google to continue</p>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
     </div>
+  );
+};
